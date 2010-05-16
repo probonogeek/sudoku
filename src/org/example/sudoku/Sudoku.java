f633fc1edf4cb2c65ba7b9b24d53fba4eeafa147
Sean Kellogg <skellogg@trilogyinteractive.com>
2010-05-15 18:11:27 -0700

    Exercise 3.11

--- src/org/example/sudoku/Sudoku.java
+++ src/org/example/sudoku/Sudoku.java
@@ -78,6 +78,9 @@ public class Sudoku extends Activity implements OnClickListener {
 		    case R.id.new_button:
 		    	openNewGameDialog();
 		    	break;
+		    case R.id.exit_button:
+		    	finish();
+		    	break;
 	    	// More buttons go here (if any) ...
     	}
 	}
